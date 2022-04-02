1c75ef9119ae4d43d4ba972a4e09327b0665c8d8
adadesions <adadesions@gmail.com>
2022-04-02 16:28:24 +0700

    Add Scanf

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "stdio.h"
 
 int main() {
-    printf("AdaBrain");
+    printf("How old are you?\n");
+    int age = 0;
+    scanf("%d", &age);
+
+    printf("You are %d years old\n", age);
+    printf("Address %p\n", &age);
 
     return 0;
 }
